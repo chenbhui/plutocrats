589d58699aa9f32340b7f520efc42d62100051c2
cyw <653502212@qq.com>
2022-07-25 11:24:14 +0800

    提交

---
+++ src/components/index.js
@@ -0,0 +1,18 @@
+//全局注册
+
+let obj = {
+
+}
+
+function install(Vue){
+    Object.keys(obj).forEach(key=>{
+         Vue.component(key,obj[key])
+    })
+}
+
+let API = {
+    version:'1.0',
+    install
+}
+
+export default API
---
+++ src/views/comps/leftViews.vue
@@ -0,0 +1,47 @@
+
+<template>
+ <div class="wrapper">
+     物料
+ </div>
+</template>
+
+<script>
+
+export default {
+    components: {
+
+    },
+    props: {
+
+    },
+    data() {
+
+    },
+    computed: {
+
+    },
+    created() {
+
+    },
+    mounted() {
+
+    },
+    methods: {
+      dragStart(e) {
+
+
+      }
+    },
+    watch: {
+
+    },
+
+}
+</script>
+
+<style scoped>
+.wrapper{
+    width: 243px;
+    padding:8px;
+}
+</style>

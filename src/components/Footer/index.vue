32c88c7688f017ca9c390264e1d325381eaad205
陈冰慧 <chenbinghuibbh@126.com>
2022-07-29 00:18:59 +0800

    改了header&Footer的样式

--- src/components/Footer/index.vue
+++ src/components/Footer/index.vue
@@ -1,24 +1,22 @@
 <template>
-  <div class="footer">
-    <h5>Copyright © | All Rights Reserved</h5>
-    <h6>love and expectation are ahead</h6>
-  </div>
+  <footer class="footer">
+    <div class="footer-publish">
+      <p class="copyright">Copyright &copy; | All Rights Reserved</p>
+      <p class="slogan">love and expectation are ahead</p>
+    </div>
+  </footer>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style lang="less" scoped>
-
-
 .footer {
   position: relative;
   bottom: 0;
   left: 0;
-  height: 50px;
+  height: 80px;
   background-color: #5176ab;
   text-align: center;
   h5 {
@@ -26,6 +24,23 @@ export default {
   }
   h6 {
     line-height: 20px;
+  }
+}
+
+/* 尾部 */
+.footer {
+  width: 100%;
+  background: #5176ab;
+  .footer-publish {
+    width: 100%;
+    height: 100px;
+    padding-top: 30px;
+    text-align: center;
+    background: #5176ab;
+    .copyright {
+      margin-bottom: 10px;
+      font-size: 16px;
+    }
   }
 }
 </style>
--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -1,72 +1,136 @@
 <template>
-  <div class="header">
-    <!-- logo -->
-      <div class="logo">
-        <img src="@/assets/images/logo.jpg" alt="">
+  <!-- 头部 -->
+  <header class="header">
+    <div class="wrap header-wrap">
+      <div class="title-area">
+        <router-link to="/home">
+          <img src="../../assets/images/logo.png" alt="" />
+        </router-link>
       </div>
-      <!-- 导航栏部分 -->
-      <ul class="nav">
-        <li><a href="#">PSY SERVICE</a></li>
-        <li><a href="#">DECOMPRESSION</a></li>
-        <li><a href="#">PSY KNOWLEDGE</a></li>
-        <li><a href="#">ABOUT US</a></li>
-      </ul>
-      <!-- 头像 -->
-      <div class="author">
-        <img src="@/assets/images/author.jpg" alt="">
+      <div class="widget-area">
+        <ul class="nav">
+          <li><router-link to="/chatroom">PSY SERVICE</router-link></li>
+          <li><a href="javascript:;">DECOMPRESSION</a></li>
+          <li><router-link to="/knowledge">PSY KNOWLEDGE</router-link></li>
+          <li><router-link to="/aboutus">ABOUT US</router-link></li>
+          <!-- router-link是声明式导航，无法有其他业务逻辑，只能跳转，编程式导航（replace、push）才能有其他业务 -->
+          <li>
+            <router-link to="login" v-if="!showUser">SIGN IN</router-link>
+          </li>
+          <li class="authorPart" v-if="showUser">
+            <div class="authorImg" @click="changeshowexit">
+              <img src="../../assets/images/author.jpg" alt="" />
+            </div>
+            <div class="authorPartItem">
+              <p class="myself" v-show="showexit" @click="showMyself">
+                我的资料
+              </p>
+              <p class="exit" v-show="showexit" @click="exitload">退出</p>
+            </div>
+          </li>
+        </ul>
       </div>
-  </div>
+    </div>
+  </header>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style lang="less" scoped>
 .header {
-  height: 50px;
-  background-color: #5176ab;
-
-  .logo {
-    float: left;
-    height: 50px;
-    width: 200px;
-    margin-left: 80px;
-    img{
-      height: 100%;
-    }
-  }
-
-  .nav {
-    float: left;
+  width: 100%;
+  background: #5377ac;
+  height: 80px;
+  .wrap {
+    width: 1300px;
     height: 100%;
-    margin: 0 0 0 200px;
-    li {
+    margin: 0 auto;
+    max-width: 1300px;
+    padding: 10px;
+    .title-area {
       float: left;
-      margin: 0 15px;
-      line-height: 50px;
+      display: block;
+      position: relative;
+      top: -6px;
+      width: 250px;
+      max-width: 250px;
+      height: 100%;
       a {
-        font-size: 16px;
-        color: #fff;
+        width: 100%;
+        height: 100%;
+      }
+      img {
+        width: 97%;
+        height: 122%;
       }
     }
   }
+}
 
-  .author {
-    float: right;
-    margin: 5px 100px 0 0;
-    height: 40px;
-    width: 40px;
-    border-radius: 50%;
-    overflow: hidden;
-    border: 1px solid #fff;
-    img {
-      width: 100%;
+.widget-area {
+  float: right;
+  width: 760px;
+  max-width: 760px;
+  height: 100%;
+  .nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 100%;
+    li {
       height: 100%;
-      border-radius: 50%;
+      a {
+        height: 100%;
+        line-height: 60px;
+        font-size: 17px;
+        letter-spacing: 1px;
+      }
     }
   }
+}
+
+.nav li.authorPart {
+  position: relative;
+  display: flex;
+  height: 50px;
+  cursor: pointer;
+  div.authorImg {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    border: 2px solid #fff;
+    overflow: hidden;
+    div img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+  div.authorName {
+    line-height: 45px;
+  }
+}
+
+.authorPartItem {
+  position: absolute;
+  top: 65px;
+  left: -9px;
+  width: 80px;
+  border-radius: 0 0 15px 15px;
+  background: #5377ac;
+  overflow: hidden;
+  z-index: 9999;
+}
+.authorPartItem p.exit,
+.authorPartItem p.myself {
+  width: 100%;
+  height: 30px;
+  text-align: center;
+  line-height: 30px;
+  color: #fff;
+  font-weight: bold;
+  letter-spacing: 2px;
+  cursor: pointer;
 }
 </style>
